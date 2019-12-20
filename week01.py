6af51a29e37c86f6558761e662e68f2b4e306256
Daniel Tan <dtan@tripadvisor.com>
2019-12-20 12:54:59 +0800

    got pymc3 working with pyenv

---
+++ week01.py
@@ -0,0 +1,13 @@
+#%%
+import numpy as np
+import pandas as pd
+
+#%%
+import pymc3 as pm
+# I can't believe the blood sweat and tears 
+# that had to go into making this run
+
+# %%
+print(1)
+
+# %%
